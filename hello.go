dff5747843fae8c71ffd1531703f88e46b5849a1
ryokosuge <ryo.kosuge@gmail.com>
2020-05-06 17:15:24 +0900

    create Reverse

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/ryokosuge/hello/morestrings"
+)
 
 func main() {
-	fmt.Println("Hello World.")
+	fmt.Println(morestrings.ReverseRunes("Hello Go!"))
 }
